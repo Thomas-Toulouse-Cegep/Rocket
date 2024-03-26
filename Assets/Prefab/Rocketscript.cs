bf69367745553d53ca127132eb13327bfb25af67
TToulouse <2134063@cegepba.qc.ca>
2024-03-26 13:32:22 -0400

    funny movement

--- Assets/Prefab/Rocketscript.cs
+++ Assets/Prefab/Rocketscript.cs
@@ -1,36 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Rigidbody))]
-public class Rocketscript : MonoBehaviour
+public class RocketMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidBody;
-    [SerializeField] private float _speed = 1000;
-    // Start is called before the first frame update
+    [SerializeField] private float thrustForce = 10f;
+    [SerializeField] private float rotationSpeed = 100f;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        _rigidBody = GetComponent<Rigidbody>();
-
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         GameObject camera = GameObject.Find("Main Camera");
-        camera.transform.position = new Vector3(transform.position.x, transform.position.y + 4, transform.position.z - 10); 
-        float spaceBar = Input.GetAxis("Jump");
-        float deltaTime = Time.deltaTime;
-        Vector3 force = new Vector3(0, spaceBar, 0) * _speed * Time.deltaTime;
-     
-        _rigidBody.AddRelativeForce(force,ForceMode.Impulse);
-        _rigidBody.AddRelativeTorque(force,ForceMode.Impulse);
+        // add offset to camera so you can see the player
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y + 4, transform.position.z - 10);
         
-        // 
-        
-        
-        
+        // Rotation
+        float yaw = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        float pitch = Input.GetAxis("Vertical") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(-pitch, yaw, 0f);
 
-       //
-
+        // Thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.forward * (thrustForce * Time.deltaTime));
+        }
     }
 }
